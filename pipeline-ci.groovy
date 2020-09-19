cf76d094ed8a1c06735282b1780e0e1415fbfbf2
bezarsnba <bezarsnba@gmail.com>
2020-09-19 17:26:15 +0000

    Primeiro commit

---
+++ pipeline-ci.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Fazendo a build do projeto'
+            }
+        }
+        stage('Unit Tests') {
+            steps {
+                echo 'Fazer testes unitários'
+            }
+        }
+        stage('Deploy to Stage') {
+            steps {
+                echo 'Fazer deploy em ambiente de homologação (staging).'
+            }
+        }
+        stage('Acceptance Tests') {
+            steps {
+                echo 'Fazer deploy em ambiente de homologação (staging).'
+            }
+        }          
+    }
+}
