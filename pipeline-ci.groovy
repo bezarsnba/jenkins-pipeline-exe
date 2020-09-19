8b02e5c4752ffdf6ca3eef58844f70dddaae94e4
Beza <bezarsnba@gmail.com>
2020-09-19 14:57:14 -0300

    Update pipeline-ci.groovy

--- pipeline-ci.groovy
+++ pipeline-ci.groovy
@@ -1,45 +1,18 @@
+#!/usr/bin/env groovy
 pipeline {
-    //Agent é o NÓ que vai rodar o job
     agent any
 
-    //Fases do pipeline
     stages {
         stage('Build') {
             steps {
                 echo 'Fazendo a build do projeto'
             }
         }
-
-        parallel {
-            stage('Teste unitários') {
-                agent {
-                    agent any
-                }
-                steps {
-                    echo "Trecho 1"
-                }
+        stage('Unit Tests') {
+            steps {
+                echo 'Fazer testes unitários'
             }
-
-            stage('Testes de aceitação') {
-                agent {
-                    agent any
-                }
-                steps {
-                    echo "Trecho 2"
-                }
-            }
-
-            stage('Testes de negócio') {
-                agent {
-                    agent any
-                }
-                steps {
-                    echo "Trecho 3"
-                }
-            }
-
         }
-
         stage('Deploy to Stage') {
             steps {
                 echo 'Fazer deploy em ambiente de homologação (staging).'
@@ -49,6 +22,6 @@ pipeline {
             steps {
                 echo 'Fazer deploy em ambiente de homologação (staging).'
             }
-        }          
+        }
     }
 }
